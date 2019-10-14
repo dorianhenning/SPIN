9c828e07287e994f0a67f9cf4b9a3c56086dcd3d
Dorian Henning <dorian.henning@gmail.com>
2019-10-14 18:57:24 +0100

    * fixed compatibility issues with scipy * scipy imresize depricated since 1.0.0 * usage of PIL Image resize * added numpy dtype to new_img contructor in crop() to avoid wrong typecasting

--- utils/imutils.py
+++ utils/imutils.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import scipy.misc
 import cv2
+from PIL import Image
 
 import constants
 
@@ -60,7 +61,7 @@ def crop(img, center, scale, res, rot=0):
     new_shape = [br[1] - ul[1], br[0] - ul[0]]
     if len(img.shape) > 2:
         new_shape += [img.shape[2]]
-    new_img = np.zeros(new_shape)
+    new_img = np.zeros(new_shape, dtype='uint8')
 
     # Range to fill new array
     new_x = max(0, -ul[0]), min(br[0], len(img[0])) - ul[0]
@@ -76,7 +77,7 @@ def crop(img, center, scale, res, rot=0):
         new_img = scipy.misc.imrotate(new_img, rot)
         new_img = new_img[pad:-pad, pad:-pad]
 
-    new_img = scipy.misc.imresize(new_img, res)
+    new_img = np.array(Image.fromarray(new_img).resize(res))
     return new_img
 
 def uncrop(img, center, scale, orig_shape, rot=0, is_rgb=True):
