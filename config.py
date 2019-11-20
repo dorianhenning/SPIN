d59ea08d655d90aae862848c3c2f30d5bfcb07cb
Dorian Henning <dorian.henning@gmail.com>
2019-11-20 10:34:43 +0000

    * absolute config paths (since SPIN ist changed to be a package)

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ PW3D_ROOT = ''
 UPI_S1H_ROOT = ''
 
 # Output folder to save test/train npz files
-DATASET_NPZ_PATH = 'data/dataset_extras'
+DATASET_NPZ_PATH = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/dataset_extras'
 
 # Output folder to store the openpose detections
 # This is requires only in case you want to regenerate 
 # the .npz files with the annotations.
-OPENPOSE_PATH = 'datasets/openpose'
+OPENPOSE_PATH = '/vol/bitbucket/dfh17/git/SPIN/SPIN/datasets/openpose'
 
 # Path to test/train npz files
 DATASET_FILES = [ {'h36m-p1': join(DATASET_NPZ_PATH, 'h36m_valid_protocol1.npz'),
@@ -53,10 +53,11 @@ DATASET_FOLDERS = {'h36m': H36M_ROOT,
                    'upi-s1h': UPI_S1H_ROOT,
                 }
 
-CUBE_PARTS_FILE = 'data/cube_parts.npy'
-JOINT_REGRESSOR_TRAIN_EXTRA = 'data/J_regressor_extra.npy'
-JOINT_REGRESSOR_H36M = 'data/J_regressor_h36m.npy'
-VERTEX_TEXTURE_FILE = 'data/vertex_texture.npy'
-STATIC_FITS_DIR = 'data/static_fits'
-SMPL_MEAN_PARAMS = 'data/smpl_mean_params.npz'
-SMPL_MODEL_DIR = 'data/smpl'
+CUBE_PARTS_FILE = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/cube_parts.npy'
+JOINT_REGRESSOR_TRAIN_EXTRA = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/J_regressor_extra.npy'
+JOINT_REGRESSOR_H36M = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/J_regressor_h36m.npy'
+VERTEX_TEXTURE_FILE = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/vertex_texture.npy'
+STATIC_FITS_DIR = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/static_fits'
+SMPL_MEAN_PARAMS = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/smpl_mean_params.npz'
+SMPL_MODEL_DIR = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/smpl'
+SPIN_MODEL = '/vol/bitbucket/dfh17/git/SPIN/SPIN/data/model_checkpoint.pt'
