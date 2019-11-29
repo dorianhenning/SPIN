834dc3729c44f5cbed9eb24fc53f387d3f9af939
Dorian Henning <dorian.henning@gmail.com>
2019-11-29 16:27:06 +0000

    * example state dict loader for changed model that has a new head

--- train.py
+++ train.py
@@ -1,6 +1,19 @@
 from utils import TrainOptions
 from train import Trainer
 
+# Load own predtrained model although model state dict changed
+def load_my_state_dict(self, state_dict):
+
+    own_state = self.state_dict()
+    for name, param in state_dict.items():
+        if name not in own_state:
+             continue
+        if isinstance(param, Parameter):
+            # backwards compatibility for serialized parameters
+            param = param.data
+        own_state[name].copy_(param)
+
+
 if __name__ == '__main__':
     options = TrainOptions().parse_args()
     trainer = Trainer(options)
